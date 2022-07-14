d1ed1870f66822b30dd657f3f9733c0c11b17470
StephenHenryJr <shenryjr_85@hotmail.com>
2022-07-13 23:28:48 -0400

    added playing class to key element

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 window.addEventListener('keydown', (e) => {
-    console.log(e)
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`); 
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`); 
+    if(!audio) return; // will end function 
+    audio.currentTime = 0; // will reset our audio file to the start
+    audio.play(); //this method will play the audio captured in our html audio element
+    key.classList.add('playing') // adds class of playing to our key element
 })
